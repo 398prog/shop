ce5a2914e8cadfaf9af5a0d1b11ca003f40ceb9a
398 <51398201+398prog@users.noreply.github.com>
2021-06-08 18:02:33 +0900

    6/8
    JSP,CRUD,JPQL

--- src/main/java/jp/co/sss/shop/controller/SessionController.java
+++ src/main/java/jp/co/sss/shop/controller/SessionController.java
@@ -1,6 +1,9 @@
 package jp.co.sss.shop.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -42,4 +45,42 @@ public class SessionController {
 		System.out.println("パスワード:" + form.getPassword());
 		return "session/loginUsingForm";
 	}
+
+	@RequestMapping(path = "/loginOnRequest",method = RequestMethod.GET)
+	public String loginOnRequest() {
+		return "session/loginOnRequest";
+	}
+
+	@RequestMapping(path = "/doLoginOnRequest",method = RequestMethod.POST)
+	public String doLoginOnRequest(LoginForm form,Model model) {
+		model.addAttribute("userId",form.getUserId());
+		return "session/loginOnRequest";
+	}
+
+	@RequestMapping(path = "/loginOnSession",method = RequestMethod.GET)
+	public String loginOnSession() {
+		return "session/loginOnSession";
+	}
+
+	@RequestMapping(path = "/doLoginOnSession",method = RequestMethod.POST)
+	public String doLoginOnSession(LoginForm form, HttpSession session) {
+		if (form.getUserId() == 123) {
+			//入力したユーザ ID をスコープ変数 userId に代入し、
+			//その変数をセッションに登録
+			session.setAttribute("userId", form.getUserId());
+			return "redirect:/";
+		} else {
+			return "session/loginOnSession";
+		}
+
+	}
+
+	@RequestMapping(path = "/logout", method = RequestMethod.GET)
+	public String logout(HttpSession session) {
+	//セッションの破棄
+		session.invalidate();
+		return "redirect:/";
+	}
+
+
 }
